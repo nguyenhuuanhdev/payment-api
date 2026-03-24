b942d2f89cbe7849e27c32ee36a60fae6616aabb
nguyenhuuanhdev <nguyenhuuanhvippro@gmail.com>
2026-03-24 21:46:12 +0700

    Mô tả thay đổi

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
-//xanh...
+//ngay1
 func main() {
 	r := gin.Default()
 
--- models/order.go
+++ models/order.go
@@ -1,5 +1,5 @@
 package models
-
+//1
 type Order struct {
 	ID     int     `json:"id"`
 	Amount float64 `json:"amount"`
